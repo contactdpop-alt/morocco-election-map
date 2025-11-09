f01fdbf4b303c34a3e3d71600c1f81b296d8891b
contactdpop-alt <contact.dpop@gmail.com>
2025-11-09 03:28:40 -0500

    Add projects page

---
+++ pages/projets/index.jsx
@@ -0,0 +1,8 @@
+export default function Projects() {
+  return (
+    <div style={{ padding: '1rem' }}>
+      <h1>Suivi des Projets Nationaux</h1>
+      <p>Cette page affichera des cartes chronologiques des grands projets nationaux, avec des indicateurs sur le budget, la région, l’état d’avancement et les résultats.</p>
+    </div>
+  );
+}
