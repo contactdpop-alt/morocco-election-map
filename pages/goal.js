8dd51f2a27e48fadac88356bd3f43dd2c1d76d74
contactdpop-alt <contact.dpop@gmail.com>
2025-11-09 01:25:33 -0500

    Create goal.js

---
+++ pages/goal.js
@@ -0,0 +1,8 @@
+export default function Goal() {
+  return (
+    <div style={{ padding: '1rem' }}>
+      <h1>Our Goal</h1>
+      <p>The goal of this platform is to provide citizens with accessible information and interactive tools about the 2026 election in Morocco. We aim to empower voters by offering data on turnout, demographics, and key issues in a neutral, engaging format.</p>
+    </div>
+  );
+}
