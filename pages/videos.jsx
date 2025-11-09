7cab45e4e57b0d4f6f617019002119a6473c11a6
contactdpop-alt <contact.dpop@gmail.com>
2025-11-09 03:30:42 -0500

    Add videos page

---
+++ pages/videos.jsx
@@ -0,0 +1,8 @@
+export default function Videos() {
+  return (
+    <div style={{ padding: '1rem' }}>
+      <h1>Vidéos</h1>
+      <p>Cette section rassemblera des vidéos explicatives et des histoires liées aux données politiques et civiques.</p>
+    </div>
+  );
+}
